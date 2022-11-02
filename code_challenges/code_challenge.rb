8e8d9927766ec9746d047c87d21003c273fb4d3d
Dawson Timmons <DawsonTimmons@gmail.com>
2022-11-02 13:45:26 -0600

    Add the conditional statement in code challenge

--- code_challenges/code_challenge.rb
+++ code_challenges/code_challenge.rb
@@ -5,3 +5,10 @@
 # String "even" if the Integer is even, and the String
 # "odd" if the Integer is odd.
 
+number = 13
+
+if number % 2 == 0
+    puts "This number is even!"
+else 
+    puts "This number is odd!"
+end
